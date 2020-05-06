9b7efb3c5953650195d1986deaa253152860c3e1
alan echer <alan.echer@gmail.com>
2020-05-06 19:28:40 -0300

    criação da tela para listagem de personagens

---
+++ app/src/main/java/br/com/alanecher/desafioandroid/ui/ActivityListagemPersonagens.kt
@@ -0,0 +1,13 @@
+package br.com.alanecher.desafioandroid.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.alanecher.desafioandroid.R
+
+class ActivityListagemPersonagens : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_listagem_personagens)
+    }
+}
